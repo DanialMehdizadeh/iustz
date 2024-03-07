fa2ee18d70c6019371fa4d6a07d8ba3af0fb6961
alirezaakalantari <154626147+alirezaakalantari@users.noreply.github.com>
2024-03-07 02:21:58 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,6 +4,93 @@
 #include <vector>
 
 using namespace std;
+// #include <iostream> 
+// #include <string> 
+
+// using namespace std ; 
+
+// class player {
+// protected : 
+// string name ;
+// string gender; 
+// //choose class and special skills and stat
+// //string class ;
+// int age ;
+// double health ; 
+// double stamina ;
+// int attackdamage ;
+// /* define number in meeting 
+//   level playerlevel ;
+//   playerlevel.level = 1 ;*/
+// /* diffrent skill 
+// int coldweapon
+// int warmweapon 
+// ...
+// */
+// public : 
+// player() {} ;
+// player(string newname,double newhealth,double newstamina,int newattackdamage,int newage,string newgender) : name(newname),
+// health(newhealth),stamina(newstamina),attackdamage(newattackdamage),age(newage),gender(newgender) {}; 
+// player(string newname){
+//     name = newname; 
+// }
+// player(double newhealth,double newstamina){
+//     health = newhealth ; 
+//     stamina = newstamina ; 
+// }
+// player (int newattackdamge){
+//     attackdamage = newattackdamge;
+// }
+// player(int newage,string newgender,string newname){
+//     age = newage ;
+//     gender = newgender ;
+//     name = newname ;
+// }
+
+// string getname() {return name ;}
+
+// //int getlevel() {return level ; }
+
+// double gethealth() {return health ;}
+
+// double getstamina() {return stamina ;}
+
+// int getattackdamage() {return attackdamage ;}
+
+// int getage() {return age ;}
+// string getgender() {return gender ;}
+
+// };
+// /*struct level 
+// struct level {
+// int levelnumber ;
+// int strength ;
+// int intelligence ;
+// int agility ;
+// };
+
+// */
+// /* function niaz
+// void getdamage (int attackdamage) { 
+// health -= attackdamage ;
+// }
+// void increaselevel(){
+//     playerlevel.level += 1 ;
+// }
+// void increasestat(int number) {
+//     if (number == 1 )
+//     playerlevel.strengt += 1 ;
+//     if (number == 2 )
+//     playerlevel.strengt += 1 ;
+//     if (number == 3 )
+//     playerlevel.strengt += 1 ;
+
+// }
+// void staminaloss(int num) {
+//     stamina -= num ; 
+// }
+
+
 
 void Story(string name) {
     cout << "there's a survivor named " << name <<".\n"
